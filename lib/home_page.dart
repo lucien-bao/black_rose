d3b9e1de268f99f6c0f4e3c64e7dd86c49838314
Violet Renard <violet.renard@proton.me>
2025-09-30 13:51:23 -0400

    feat: create generic menu button class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,14 +12,21 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Black Rose',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 50,
-                fontFamily: "Alegreya Sans SC",
-                color: const Color(0xffd600dc),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.file(File('assets/black_rose.png')),
+                SizedBox(width: 5),
+                Text(
+                  'Black Rose',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 50,
+                    fontFamily: "Alegreya Sans SC",
+                    color: const Color(0xffd600dc),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 20),
             NewGameButton(),
@@ -37,18 +46,37 @@ class NewGameButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
-
     return FilledButton.icon(
       onPressed: newGame,
       icon: const Icon(Icons.add, size: 35),
       label: Text(
         'New Game',
-        style: TextStyle(
-          fontSize: 35,
-          fontFamily: "Alegreya Sans SC",
-          color: colorScheme.onSecondary,
-        ),
+        style: TextStyle(fontSize: 35, fontFamily: "Alegreya Sans SC"),
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  const MenuButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.callbackFn,
+  });
+
+  final Icon icon;
+  final String text;
+  final void Function() callbackFn;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton.icon(
+      onPressed: callbackFn,
+      icon: icon,
+      label: Text(
+        text,
+        style: TextStyle(fontSize: 35, fontFamily: "Alegreya Sans SC"),
       ),
     );
   }
