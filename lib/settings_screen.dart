624786f51125bc4db3d6071fbec905ea9beacf4e
Violet Renard <violet.renard@proton.me>
2025-10-02 11:21:13 -0400

    feat: create settings screen

---
+++ lib/settings_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  double getMenuWidth(double screenWidth) {
+    if (screenWidth > 800) {
+      return 400;
+    }
+    if (screenWidth > 200) {
+      return screenWidth * 0.5;
+    }
+    return screenWidth - 100;
+  }
+
+  double getMarginWidth(double screenWidth) =>
+      (screenWidth - getMenuWidth(screenWidth)) / 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SizedBox(width: getMarginWidth(constraints.maxWidth)),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    LogoTitle(),
+
+                    SizedBox(height: 20),
+                    Text("settings screen"),
+                  ],
+                ),
+              ),
+              SizedBox(width: getMarginWidth(constraints.maxWidth)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class LogoTitle extends StatelessWidget {
+  const LogoTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.file(File('assets/black_rose.png')),
+        SizedBox(width: 5),
+        Text(
+          'Settings',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 50,
+            fontFamily: 'Alegreya Sans SC',
+            color: const Color(0xffd600dc),
+          ),
+        ),
+      ],
+    );
+  }
+}
